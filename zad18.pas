36867e154541a879105d0f0b111ea37a8b43108e
Pablo257-lol <126115185+Pablo257-lol@users.noreply.github.com>
2023-10-03 14:48:44 +0300

    Add files via upload

--- zad18.pas
+++ zad18.pas
@@ -3,6 +3,12 @@ var a:integer;
 begin
 writeln('введите год');
 read(a);
-if a mod 4=0 then write('високосный год')
+if a mod 4=0 then 
+begin
+  if a mod 400=0 then
+  write('високосный год')
+  else
+    write('не високосный')
+  end
 else write('не високосный год');
 end.
